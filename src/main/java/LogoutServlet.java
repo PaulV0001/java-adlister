edc56b0b552f93312bcfe2cee32ab5c8c60224b2
paulvaldez <valdezpaul00@gmail.com>
2017-10-19 12:00:21 -0500

    Completed the exercises for the sessions and cookies exercise.

---
+++ src/main/java/LogoutServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = "/login")
+public class LogoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        request.getSession().removeAttribute("user");
+        request.getSession().invalidate();
+        response.sendRedirect("login");
+
+        out.println("You are successfully logged out!");
+    }
+}
