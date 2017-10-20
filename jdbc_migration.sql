f45970499b8176dfc6b0b6a242020ab31491af78
paulvaldez <valdezpaul00@gmail.com>
2017-10-20 11:18:50 -0500

    Created the migration file with a user and ads table.

---
+++ jdbc_migration.sql
@@ -0,0 +1,16 @@
+USE adlister_db;
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  title VARCHAR(50) NOT NULL,
+  description TEXT NOT NULL
+);
