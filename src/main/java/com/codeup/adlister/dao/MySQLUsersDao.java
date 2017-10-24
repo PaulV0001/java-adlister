5a7d4fa95a791719ed08965dc3804a6a5acbdd92
paulvaldez <valdezpaul00@gmail.com>
2017-10-24 11:15:03 -0500

    Create factory method for the Dao of Users.

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,15 +1,68 @@
 package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.jdbc.PreparedStatement;
+
+import java.sql.*;
 
 public class MySQLUsersDao implements Users {
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+
     @Override
     public User findByUsername(String username) {
-        return null;
+        String sql = "SELECT * FROM users WHERE username = ?";
+        try {
+        PreparedStatement statement = (PreparedStatement) connection.prepareStatement(sql);
+        statement.setString(1, username);
+        ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                return new User(
+                        resultSet.getLong("id"),
+                        resultSet.getString("username"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password")
+                );
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+            return null;
     }
 
     @Override
     public Long insert(User user) {
-        return null;
+        String sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement statement = (PreparedStatement) connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            statement.executeUpdate();
+
+            ResultSet resultSet = statement.getGeneratedKeys();
+            resultSet.next();
+            return resultSet.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
