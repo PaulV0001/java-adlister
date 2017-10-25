a0ef04dece083552bc6b8ee78b3b5fa548eaf681
paulvaldez <valdezpaul00@gmail.com>
2017-10-25 09:06:06 -0500

    Don't allow the creation of ads unless logged in.

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,6 +25,7 @@ public class CreateAdServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User user = (User) request.getSession().getAttribute("user");
 
         if (request.getSession().getAttribute("user") == null) {
             response.sendRedirect("/login");
@@ -31,6 +33,7 @@ public class CreateAdServlet extends HttpServlet {
         }
 
         Ad ad = new Ad(
+                user.getId(),
             1, // for now we'll hardcode the user id
             request.getParameter("title"),
             request.getParameter("description")
